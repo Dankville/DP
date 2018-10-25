6eb54f340c3e89afc842d515c84b996b64fa1fa7
Fib\Marco <mpost1995@gmail.com>
2018-10-25 09:30:28 +0200

    Flyweight started

---
+++ Flyweight/Flyweight/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+	class Circle : Shape
+	{
+		private string _Color;
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Radius { get; set; }
+
+		private Circle(string color)
+		{
+			this._Color = color;
+		}
+
+		public void Draw()
+		{
+			Console.WriteLine($"Circle: Draw() [Color: {_Color}, X: {X}, Y: {Y}]");
+		}
+	}
+}
---
+++ Flyweight/Flyweight/ShapeFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+	class ShapeFactory
+	{
+		private static 
+
+		public static Shape GetCricle(string color)
+		{
+			Circle circle = 
+		}
+	}
+}
